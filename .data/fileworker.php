16684f07712e16299306f54517d8de93669481cf
Geartwo <geartwo@chrometech.at>
2017-02-10 15:34:24 +0100

    Fileworker fail

--- .data/fileworker.php
+++ .data/fileworker.php
@@ -2,38 +2,30 @@
 //Rename
 $folder = workpath($_GET['f']);
 if(isset ($_GET['renold']) && isset ($_GET['rennew'])){
-$newf = $_GET['newf'];
-if ($isad('edit')) {
-$old =$_GET['renold'];
-$new =$_GET['rennew'];
-	if(preg_match("/\.mp3\z/i", $old) && !preg_match("/\.mp3\z/i", $new)):
-		exec('ffmpeg -i "'.$folder.'/'.$old.'" "'.$folder.'/'.$new.'" > /dev/null');
-		unlink($folder."/".$old);
-	elseif(preg_match("/\.wma\z/i", $old) && !preg_match("/\.wma\z/i", $new)):
-		exec('ffmpeg -i "'.$folder.'/'.$old.'" "'.$folder.'/'.$new.'" > /dev/null');
-		unlink($folder."/".$old);
-	else:
-		rename($folder."/".$old, $newf."/".$new);
-	endif;
-                        rename($folder."/.pic_".$old.".jpg", $newf."/.pic_".$new.".jpg");
-                        $df = implode('/', explode('%2F', rawurlencode($folder)));
-			$new = $db->real_escape_string($new);
-                        $newf = $db->real_escape_string($newf);
-                        $df = $db->real_escape_string($df);
-                        $old = $db->real_escape_string($old);
-                        $db->query("UPDATE files SET name = '$new', folder= '$newf' WHERE folder = '$df' AND name = '$old'");
-                }else{
+	$newf = workpath($_GET['newf']);
+	if ($isad('edit')) {
+		$old =$_GET['renold'];
+		$new =$_GET['rennew'];
+		rename(".$folder$old", ".$newf$new");
+        	rename($folder."/.pic_".$old.".jpg", $newf."/.pic_".$new.".jpg");
+                $df = implode('/', explode('%2F', rawurlencode($folder)));
+		$new = $db->real_escape_string($new);
+                $newf = $db->real_escape_string($newf);
+                $df = $db->real_escape_string($df);
+                $old = $db->real_escape_string($old);
+                $db->query("UPDATE files SET name = '$new', folder= '$newf' WHERE folder = '$df' AND name = '$old'");
+        }else{
                         echo $lang->norenameright;
                 }
-}elseif (isset ($_GET['newfolder']) && $isad('edit')){
+}elseif(isset ($_GET['newfolder']) && $isad('edit')){
                 $new = $_GET['newfolder'];
 		error_log(".$folder$new");
                 mkdir(".$folder$new", 0755);
-}elseif (isset ($_GET['delfile']) && $isad('edit')){
+}elseif(isset ($_GET['delfile']) && $isad('edit')){
 	$del = $_GET['delfile'];
 	unlink(".$folder$del");
 	$db->query("DELETE FROM files WHERE folder = '$folder' AND name='$del'");
-}elseif (isset ($_GET['deldir']) && $isad('deldir')){
+}elseif(isset ($_GET['deldir']) && $isad('deldir')){
 	$del = $_GET['deldir'];
         rmdir(".$folder$del");
 	$db->query("DELETE FROM files WHERE folder = '$folder' AND name='$del'");
