f217d9e13088f7db5ed5ca675a1e02a7a4b2cdd6
Geartwo <geartwo@chrometech.at>
2017-02-10 14:36:09 +0100

    PHP7 old ereg to preg_match

--- .data/stream.php
+++ .data/stream.php
@@ -3,12 +3,12 @@ if(isset($_GET['watchfile'])) $cmsfolder = workpath($_GET['watchfile']);
 if (!isset($_SESSION['loggedin']) && !preg_match('/(\.pic|.bintro)*/', $cmsfolder)):
     exit;
 endif;
-if (ereg(".pdf", $cmsfolder)){header("Content-Type: application/pdf");
-} elseif (ereg(".mp4", $cmsfolder)){header("Content-Type: video/mp4");
+if (preg_match("/\.pdf/", $cmsfolder)){header("Content-Type: application/pdf");
+} elseif (preg_match("/\.mp4/", $cmsfolder)){header("Content-Type: video/mp4");
 header("X-Accel-Buffering: no");
-} elseif (ereg(".mp3", $cmsfolder)){header("Content-Type: audio/mp3");
-} elseif (ereg(".jpg", $cmsfolder)){header("Content-Type: image/jpeg");
-} elseif (ereg(".epub", $cmsfolder)){header("Content-Type: application/epub+zip");
+} elseif (preg_match("/\.mp3/", $cmsfolder)){header("Content-Type: audio/mp3");
+} elseif (preg_match("/\.jpg/", $cmsfolder)){header("Content-Type: image/jpeg");
+} elseif (preg_match("/\.epub/", $cmsfolder)){header("Content-Type: application/epub+zip");
 }
 header("X-Accel-Redirect:$cmsfolder");
 header("Cache-Control: max-age=2592000");
